ba38e58b682d4ed921aca447729584387fd8b43b
JaeZheng <846265697@qq.com>
2018-12-24 00:26:21 +0800

    add insertProdcutRecord

--- src/main/java/scut/bigproject/super_erp/controller/ProductRecordController.java
+++ src/main/java/scut/bigproject/super_erp/controller/ProductRecordController.java
@@ -1,24 +1,41 @@
-package scut.bigproject.super_erp.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import scut.bigproject.super_erp.service.ProductRecordService;
-
-@Controller
-@RequestMapping(value = "/productRecord")
-public class ProductRecordController {
-    @Autowired
-    ProductRecordService productRecordService;
-
-    /***
-     * 查询生产记录
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/find", method = RequestMethod.GET)
-    @ResponseBody
-    public Object tutorial(@RequestParam(value = "id", required = true) int id){
-        return productRecordService.findProductionRecord(id);
-    }
-}
+//package scut.bigproject.super_erp.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//import scut.bigproject.super_erp.entity.MaterialRecord;
+//import scut.bigproject.super_erp.entity.ProductionRecord;
+//import scut.bigproject.super_erp.entity.WorklineRecord;
+//import scut.bigproject.super_erp.entity.returnentity.Result;
+//import scut.bigproject.super_erp.service.ProductRecordService;
+//import scut.bigproject.super_erp.util.EntityUtil;
+//
+//import java.util.ArrayList;
+//import java.util.Map;
+//
+//@Controller
+//@RequestMapping(value = "/productRecord")
+//public class ProductRecordController {
+//    @Autowired
+//    ProductRecordService productRecordService;
+//
+//    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Result insertProductRecord(@RequestBody Map<String, Object> map){
+//        ProductionRecord productionRecord = EntityUtil.object2Entity(
+//                map.get("productionRecord"), ProductionRecord.class);
+//
+//
+//    }
+//    /***
+//     * 查询生产记录
+//     * @param id
+//     * @return
+//     */
+//
+//    @RequestMapping(value = "/findProductRecordById", method = RequestMethod.GET)
+//    @ResponseBody
+//    public Object tutorial(@RequestParam(value = "id", required = true) int id){
+//        return productRecordService.findProductionRecord(id);
+//    }
+//}
